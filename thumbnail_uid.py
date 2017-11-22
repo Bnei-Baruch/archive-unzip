6acab26d8ae646a12d5be7fab46da615f57ea7fc
yosefy <yosef.yudilevich@gmail.com>
2017-11-22 05:49:35 +0200

    return png in response

--- thumbnail_uid.py
+++ thumbnail_uid.py
@@ -5,21 +5,25 @@ from subprocess import call
 from urllib import request
 
 from flask.helpers import make_response
+from flask import send_file
 
-app_dir = 'thumbnail/'
+app_dir = 'thumbnail'
 
 
 def thumbnail_uid(uid, linker_base_url, base_dir):
     uid_dir = os.path.join(base_dir,app_dir, uid)
-    os.makedirs(uid_dir, exist_ok=True)
-    call(["ffmpeg", '-y',
-          "-i", linker_base_url + uid + ".mp4",
-          "-vf", "thumbnail=12",
-          "-vframes", "1",
-          "-vcodec", "png",
-          uid_dir + "/" + uid + ".png"])
+    png_file = os.path.join(uid_dir,uid + ".png")
 
-    return base_dir + "/" + app_dir + uid + ".mp4"
+
+    if not os.path.exists(png_file):
+        os.makedirs(uid_dir, exist_ok=True)
+        call(["ffmpeg", '-y',
+              "-i", linker_base_url + uid + ".mp4",
+              "-vf", "thumbnail=12",
+              "-vframes", "1",
+              "-vcodec", "png",
+              png_file])
+    return send_file(png_file, mimetype='image/png')
 
 
 
