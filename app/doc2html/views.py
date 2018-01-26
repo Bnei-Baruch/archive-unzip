4d630dc7058f0751588327c3537e1d9da4bb661d
berkov <berkov>
2018-01-26 07:37:39 +0200

    smarter caching of html->docx->doc

--- app/doc2html/views.py
+++ app/doc2html/views.py
@@ -17,54 +17,57 @@ def doc2html(uid):
     if file_path:
         return current_app.sendfile.send_file(file_path)
     else:
-        return make_response("missing info", 404)
+        return make_response("Missing info for uid " + uid, 404)
 
 
 def process_uid(uid):
     base_dir = current_app.config['BASE_DIR']
-    output_dir = os.path.join(base_dir, MODULE_DIR)
-    os.makedirs(output_dir, exist_ok=True)
-
-    uid_dir = os.path.join(output_dir, uid)
-    html_file = os.path.join(uid_dir, uid + '.html')
-
-    # file already exist ?
-    if os.path.exists(html_file):
-        return html_file
-
-    # make sure uid_dir exist
-    os.makedirs(uid_dir, exist_ok=True)
-
-    # get real file type from mdb
-    file_type = get_file_type(uid)
-    if not file_type:
-        return None
-
-    # Download file if necessary
-    path = os.path.join(uid_dir, '{}.{}'.format(uid, file_type))
-    if not os.path.exists(path):
-        url = current_app.config['LINKER_URL'] + uid
-        # TODO: handle http errors and map them to relevant errors to our users
-        request.urlretrieve(url, path)
-
-    # Convert doc to docx if necessary
-    if file_type == 'doc':
-        soffice_bin = current_app.config['SOFFICE_BIN']
-        path = doc_to_docx(path, soffice_bin, current_app.logger)
-
-    # Convert docx to html
-    html_file = path[:-4] + "html"
-    return docx_to_html(path, html_file, current_app.logger)
+    uid_dir = os.path.join(base_dir, MODULE_DIR, uid)
+    return get_html(uid_dir, uid)
 
 
-def get_file_type(uid):
+def get_html(uid_dir, uid):
+    html_path = build_file_path(uid_dir, uid, "html")
+    if not os.path.exists(html_path):
+        docx_path = get_docx_path(uid_dir, uid)
+        docx_to_html(docx_path, html_path, current_app.logger)
+    return html_path
+
+
+def get_docx_path(uid_dir, uid):
+    docx_path = build_file_path(uid_dir, uid, "docx")
+
+    if not os.path.exists(docx_path):
+        os.makedirs(uid_dir, exist_ok=True)
+
+        uid_file_type = get_uid_file_type(uid)
+
+        if uid_file_type == "docx":
+            fetch_uid(uid, docx_path)
+        elif uid_file_type == "doc":
+            doc_path = build_file_path(uid_dir, uid, "doc")
+
+            if not os.path.exists(doc_path):
+                fetch_uid(uid, doc_path)
+                soffice_bin = current_app.config['SOFFICE_BIN']
+                doc_to_docx(doc_path, soffice_bin, current_app.logger)
+        else:
+            current_app.logger.warn("Invalid file type for uid {}: {}".format(uid, uid_file_type))
+            return None
+
+    return docx_path
+
+
+def get_uid_file_type(uid):
     with current_app.mdb.get_cursor() as cur:
-        cur.execute("select name, type, sub_type, mime_type from files where uid = %s", (uid,))
-        d = cur.fetchone()
-        if not d:
-            return None
+        d = cur.execute("select name, type, sub_type, mime_type from files where uid = %s", (uid,)).fetchone()
+        return d['name'].split('.')[-1] if d and d['type'] == 'text' else None
 
-        if d['type'] != 'text':
-            return None
 
-        return d['name'].split('.')[-1]
+def fetch_uid(uid, path):
+    url = current_app.config['LINKER_URL'] + uid
+    request.urlretrieve(url, path)  # TODO: handle http errors and map them to relevant errors to our users
+
+
+def build_file_path(uid_dir, uid, file_type):
+    return os.path.join(uid_dir, '{}.{}'.format(uid, file_type))
