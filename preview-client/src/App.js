383c154733eda1cf8e20df886fb8871c04664d0b
davgur <davgur.ru@gmail.com>
2021-01-01 17:01:24 +0200

    add wip

--- preview-client/src/App.js
+++ preview-client/src/App.js
@@ -6,38 +6,35 @@ import { CssBaseline } from '@material-ui/core';
 function App() {
   const [html, setHtml]   = useState();
   const [error, setError] = useState();
+  const [wip, setWip]     = useState(false);
 
   const upload = (doc) => {
+    setWip(true);
+    setHtml('');
     setError(null);
     const body = new FormData();
     body.append('doc', doc);
 
     const url = `${process.env.REACT_APP_BACKEND}doc2htmlByBLob`;
 
-    const options = {
-      method: 'POST',
-      referrerPolicy: 'no-referrer-when-downgrade',
-      //referrer: '',
-      body
-    };
+    const options = { method: 'POST', body };
     fetch(url, options)
       .then(r => {
         if (!r.ok) {
           throw new Error(r.statusText);
         }
-        return r.text();
+        return r.json();
       })
-      .then(setHtml)
+      .then((d) => setHtml(d.html))
       .catch((e) => {
         setError(e.message);
-        setHtml('');
-      });
+      }).finally(() => setWip(false));
   };
 
   return (
     <>
       <CssBaseline />
-      <Layout upload={upload} html={html} error={error} />
+      <Layout upload={upload} html={html} error={error} wip={wip} />
     </>
   );
 }
--- preview-client/src/Layout.js
+++ preview-client/src/Layout.js
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { getLanguageDirection } from 'kmedia-mdb/src/helpers/i18n-utils';
+import React, { useState } from 'react';
 import {
   FormControl,
   InputAdornment,
@@ -10,15 +9,16 @@ import {
   InputLabel,
   Input,
   Paper,
-  Button, ButtonBase, TextField, ButtonGroup, Grid, Typography
+  Button, ButtonBase, TextField, ButtonGroup, Grid, Typography, CircularProgress,
 } from '@material-ui/core';
-import { Publish } from '@material-ui/icons';
+import { FormatTextdirectionLToR, FormatTextdirectionRToL, Publish } from '@material-ui/icons';
 
-const Layout = ({ language = 'he', html, upload, error }) => {
+const dirEnum = { ltr: 'ltr', rtl: 'rtl' };
+const Layout  = ({ html, upload, error, wip }) => {
 
   const [doc, setDoc]   = useState();
   const [file, setFile] = useState();
-  const direction       = getLanguageDirection(language);
+  const [dir, setDir]   = useState(localStorage.getItem('dir') || dirEnum.ltr);
 
   const handleChangeDoc = ({ target: { value, files } }) => {
     setDoc(value);
@@ -47,11 +47,17 @@ const Layout = ({ language = 'he', html, upload, error }) => {
         </FormControl>
       </Grid>
       <Grid xs={3}>
-
+        <IconButton onClick={() => setDir(dir === dirEnum.ltr ? dirEnum.rtl : dirEnum.ltr)}>
+          {
+            dir === dirEnum.ltr
+              ? <FormatTextdirectionRToL />
+              : <FormatTextdirectionLToR />
+          }
+        </IconButton>
         <Button
           onClick={handleUpload}
           variant="contained"
-
+          disabled={!file}
         >
           Upload file
           <Publish />
@@ -64,16 +70,35 @@ const Layout = ({ language = 'he', html, upload, error }) => {
     <div>
       <div className="search-on-page--container">
         <div
-          style={{ direction, textAlign: (direction === 'ltr' ? 'left' : 'right') }}
+          style={{ direction: dir, textAlign: (dir === dirEnum.ltr ? 'left' : 'right'), padding: '1em' }}
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
     </div>
   );
 
+  const renderWip = () => {
+    return (
+      <Box style={{
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        right: '0',
+        left: '0',
+        background: 'rgba(158, 158, 158, 0.5)',
+        zIndex: '100',
+        verticalAlign: 'middle',
+        textAlign: 'center'
+      }}>
+        <CircularProgress style={{ marginTop: '2em' }} size={60} />
+      </Box>
+    );
+  };
+
   return (
     <Container maxWidth="md">
-      <Box m={2}>
+      <Box m={2} style={{ position: 'relative' }}>
+        {wip && renderWip()}
         {renderUpload()}
       </Box>
       <Paper variant="outlined" style={{ minHeight: '90vh' }}>
