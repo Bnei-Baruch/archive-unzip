87cc6b517acbc226af2c2e146065f6a9e809275d
davgur <davgur.ru@gmail.com>
2022-08-21 11:07:42 +0300

    Preview client add language (#19)
    * add lang
    
    * fix name
    
    * fix style

--- preview-client/src/Layout.js
+++ preview-client/src/Layout.js
@@ -1,115 +1,139 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-  FormControl,
-  IconButton,
-  Container,
-  Box,
-  FormHelperText,
-  Input,
-  Paper,
-  Button,
-  Grid,
-  Typography,
-  CircularProgress,
+    FormControl,
+    IconButton,
+    Container,
+    Box,
+    FormHelperText,
+    Input,
+    Paper,
+    Button,
+    Grid,
+    Typography,
+    CircularProgress, Select, MenuItem,
 } from '@material-ui/core';
-import { FormatTextdirectionLToR, FormatTextdirectionRToL, Publish } from '@material-ui/icons';
+import {FormatTextdirectionLToR, FormatTextdirectionRToL, Publish} from '@material-ui/icons';
+import {ALL_LANGUAGES, LANG_HEBREW} from "./consts.mjs";
 
-const dirEnum = { ltr: 'ltr', rtl: 'rtl' };
-const Layout  = ({ html, upload, error, wip }) => {
+const dirEnum = {ltr: 'ltr', rtl: 'rtl'};
+const defLang = localStorage.getItem('lang') || LANG_HEBREW;
+const defDir = localStorage.getItem('dir') || dirEnum.ltr
 
-  const [doc, setDoc]   = useState();
-  const [file, setFile] = useState();
-  const [dir, setDir]   = useState(localStorage.getItem('dir') || dirEnum.ltr);
+const Layout = ({html, upload, error, wip}) => {
+    const [doc, setDoc] = useState();
+    const [file, setFile] = useState();
+    const [dir, setDir] = useState(defDir);
+    const [lang, setLang] = useState(defLang);
 
-  const handleChangeDoc = ({ target: { value, files } }) => {
-    setDoc(value);
-    files.length > 0 && setFile(files[0]);
-  };
+    const handleChangeLang = e => {
+        const l = e.target.value;
+        setLang(l);
+        localStorage.setItem('lang', l)
+    };
 
-  const handleUpload = () => upload(file);
+    const handleChangeDoc = ({target: {value, files}}) => {
+        setDoc(value);
+        files.length > 0 && setFile(files[0]);
+    };
 
-  const renderUpload = () => (
-    <Grid container spacing={6} alignContent="center" alignItems="center">
-      <Grid item xs={8}>
-        <FormControl variant="outlined" fullWidth>
-          <Input
-            id="doc"
-            name="doc"
-            aria-describedby="forUploadFile"
-            type="file"
-            onChange={handleChangeDoc}
-          />
-          <FormHelperText id="forUploadFile">
-            <Typography color={error ? 'error' : 'primary'} component="span">
-              {error ? error : doc}
-            </Typography>
-          </FormHelperText>
+    const handleUpload = () => upload(file);
 
-        </FormControl>
-      </Grid>
-      <Grid item xs={4}>
-        <IconButton
-          onClick={() => setDir(dir === dirEnum.ltr ? dirEnum.rtl : dirEnum.ltr)}
-          style={{ 'marginRight': '1em' }}
-        >
-          {
-            dir === dirEnum.ltr
-              ? <FormatTextdirectionRToL />
-              : <FormatTextdirectionLToR />
-          }
-        </IconButton>
-        <Button
-          onClick={handleUpload}
-          variant="contained"
-          disabled={!file}
-        >
-          Upload file
-          <Publish />
-        </Button>
-      </Grid>
-    </Grid>
-  );
+    const renderUpload = () => (
+        <Grid container spacing={6} alignContent="center" alignItems="center">
+            <Grid item xs={7}>
+                <FormControl variant="outlined" fullWidth>
+                    <Input
+                        id="doc"
+                        name="doc"
+                        aria-describedby="forUploadFile"
+                        type="file"
+                        onChange={handleChangeDoc}
+                    />
+                    <FormHelperText id="forUploadFile">
+                        <Typography color={error ? 'error' : 'primary'} component="span">
+                            {error ? error : doc}
+                        </Typography>
+                    </FormHelperText>
 
-  const renderPreview = () => (
-    <div>
-      <div className="search-on-page--container eng_styles">
-        <div
-          style={{ direction: dir, textAlign: (dir === dirEnum.ltr ? 'left' : 'right'), padding: '1em' }}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  );
-
-  const renderWip = () => {
-    return (
-      <Box style={{
-        position: 'absolute',
-        top: '0',
-        bottom: '0',
-        right: '0',
-        left: '0',
-        background: 'rgba(158, 158, 158, 0.5)',
-        zIndex: '100',
-        verticalAlign: 'middle',
-        textAlign: 'center'
-      }}>
-        <CircularProgress style={{ marginTop: '2em' }} size={60} />
-      </Box>
+                </FormControl>
+            </Grid>
+            <Grid item xs={5}>
+                <FormControl variant="standard">
+                    <IconButton
+                        onClick={() => setDir(dir === dirEnum.ltr ? dirEnum.rtl : dirEnum.ltr)}
+                        style={{'marginRight': '1em'}}
+                    >
+                        {
+                            dir === dirEnum.ltr
+                                ? <FormatTextdirectionRToL/>
+                                : <FormatTextdirectionLToR/>
+                        }
+                    </IconButton>
+                </FormControl>
+                <FormControl variant="standard">
+                    <Button
+                        onClick={handleUpload}
+                        variant="contained"
+                        disabled={!file}
+                    >
+                        Upload file
+                        <Publish/>
+                    </Button>
+                </FormControl>
+                <FormControl variant="standard">
+                    <Select
+                        id="langs-select"
+                        value={lang}
+                        label="Language"
+                        onChange={handleChangeLang}
+                    >
+                        {ALL_LANGUAGES.map(l => <MenuItem value={l}>{l}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            </Grid>
+        </Grid>
     );
-  };
 
-  return (
-    <Container maxWidth="md">
-      <Box m={2} style={{ position: 'relative' }}>
-        {wip && renderWip()}
-        {renderUpload()}
-      </Box>
-      <Paper variant="outlined" style={{ minHeight: '90vh' }}>
-        {renderPreview()}
-      </Paper>
-    </Container>
-  );
+    const renderPreview = () => (
+        <div>
+            <div className={`search-on-page--container ${lang}_styles`}>
+                <div
+                    style={{direction: dir, textAlign: (dir === dirEnum.ltr ? 'left' : 'right'), padding: '1em'}}
+                    dangerouslySetInnerHTML={{__html: html}}
+                />
+            </div>
+        </div>
+    );
+
+    const renderWip = () => {
+        return (
+            <Box style={{
+                position: 'absolute',
+                top: '0',
+                bottom: '0',
+                right: '0',
+                left: '0',
+                background: 'rgba(158, 158, 158, 0.5)',
+                zIndex: '100',
+                verticalAlign: 'middle',
+                textAlign: 'center'
+            }}>
+                <CircularProgress style={{marginTop: '2em'}} size={60}/>
+            </Box>
+        );
+    };
+
+    return (
+        <Container maxWidth="md">
+            <Box m={2} style={{position: 'relative'}}>
+                {wip && renderWip()}
+                {renderUpload()}
+            </Box>
+            <Paper variant="outlined" style={{minHeight: '90vh'}}>
+                {renderPreview()}
+            </Paper>
+        </Container>
+    );
 };
 
 export default Layout;
---
+++ preview-client/src/consts.mjs
@@ -0,0 +1,71 @@
+export const LANG_HEBREW = 'he';
+export const LANG_ENGLISH = 'en';
+export const LANG_RUSSIAN = 'ru';
+export const LANG_SPANISH = 'es';
+export const LANG_ITALIAN = 'it';
+export const LANG_GERMAN = 'de';
+export const LANG_DUTCH = 'nl';
+export const LANG_FRENCH = 'fr';
+export const LANG_PORTUGUESE = 'pt';
+export const LANG_TURKISH = 'tr';
+export const LANG_POLISH = 'pl';
+export const LANG_ARABIC = 'ar';
+export const LANG_HUNGARIAN = 'hu';
+export const LANG_FINNISH = 'fi';
+export const LANG_LITHUANIAN = 'lt';
+export const LANG_JAPANESE = 'ja';
+export const LANG_BULGARIAN = 'bg';
+export const LANG_GEORGIAN = 'ka';
+export const LANG_NORWEGIAN = 'no';
+export const LANG_SWEDISH = 'sv';
+export const LANG_CROATIAN = 'hr';
+export const LANG_CHINESE = 'zh';
+export const LANG_PERSIAN = 'fa';
+export const LANG_ROMANIAN = 'ro';
+export const LANG_HINDI = 'hi';
+export const LANG_UKRAINIAN = 'ua';
+export const LANG_MACEDONIAN = 'mk';
+export const LANG_SLOVENIAN = 'sl';
+export const LANG_LATVIAN = 'lv';
+export const LANG_SLOVAK = 'sk';
+export const LANG_CZECH = 'cs';
+export const LANG_AMHARIC = 'am';
+export const LANG_INDONESIAN = 'id';
+export const LANG_ARMENIAN = 'hy';
+
+export const ALL_LANGUAGES = [
+    LANG_HEBREW,
+    LANG_ENGLISH,
+    LANG_RUSSIAN,
+    LANG_SPANISH,
+    LANG_ITALIAN,
+    LANG_GERMAN,
+    LANG_DUTCH,
+    LANG_FRENCH,
+    LANG_PORTUGUESE,
+    LANG_TURKISH,
+    LANG_POLISH,
+    LANG_ARABIC,
+    LANG_HUNGARIAN,
+    LANG_FINNISH,
+    LANG_LITHUANIAN,
+    LANG_JAPANESE,
+    LANG_BULGARIAN,
+    LANG_GEORGIAN,
+    LANG_NORWEGIAN,
+    LANG_SWEDISH,
+    LANG_CROATIAN,
+    LANG_CHINESE,
+    LANG_PERSIAN,
+    LANG_ROMANIAN,
+    LANG_HINDI,
+    LANG_UKRAINIAN,
+    LANG_MACEDONIAN,
+    LANG_SLOVENIAN,
+    LANG_LATVIAN,
+    LANG_SLOVAK,
+    LANG_CZECH,
+    LANG_AMHARIC,
+    LANG_INDONESIAN,
+    LANG_ARMENIAN,
+];
