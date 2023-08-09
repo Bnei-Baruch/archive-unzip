7722721dd6f29b280040b47ba96064c755cf3504
davgur <davgur.ru@gmail.com>
2023-08-09 17:03:26 +0300

    add endpoint for merge kite makors

---
+++ app/km_audio/views.py
@@ -0,0 +1,26 @@
+import http
+
+from flask import Blueprint, request
+from flask.helpers import make_response, send_file
+
+from app.km_audio.km_audio import KmAudio
+
+blueprint = Blueprint('km_audio', __name__, url_prefix='/km_audio')
+
+
+@blueprint.route('/build/<uid>')
+def km_audio_build(uid):
+    km = KmAudio(uid, request.args.get('language'))
+    if km.run():
+        return make_response("ok", http.HTTPStatus.OK)
+    else:
+        return make_response("missing info", 404)
+
+
+@blueprint.route('/file/<uid>')
+def km_audio_file(uid):
+    km = KmAudio(uid, request.args.get('language'))
+    if km.run():
+        return send_file(km.path, mimetype="audio/mp3", conditional=True)
+    else:
+        return make_response("missing info", 404)
