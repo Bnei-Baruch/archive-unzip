379a7fe128e4b3565ac3eea64a81ed2d5f4ed2f5
davgur <davgur.ru@gmail.com>
2023-08-15 05:04:58 +0300

    use send file wrapper

--- app/km_audio/views.py
+++ app/km_audio/views.py
@@ -22,7 +22,8 @@ def km_audio_build(uid):
 def km_audio_file(uid):
     km = KmAudio(uid, request.args.get('language'))
     if km.run():
-        current_app.logger.error(os.stat(km.path))
-        return send_file(km.path, mimetype="audio/mpeg", conditional=True)
+        # current_app.logger.error(os.stat(km.path))
+        # return send_file(km.path, mimetype="audio/mpeg", conditional=True)
+        return current_app.sendfile.send_file(km.path)
     else:
         return make_response("missing info", 404)
